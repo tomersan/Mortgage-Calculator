3051026e9647bea2109269b669c5ae18272df581
tomersan <tomer.san1@gmail.com>
2020-04-23 16:54:35 +0300

    Mortgage Calculator 1.1

--- Mortgage Calculator.js
+++ Mortgage Calculator.js
@@ -3,90 +3,67 @@ let Interest = document.getElementById('Interest')
 let Years = document.getElementById('Years')
 let Payment = document.getElementById('Payment')
 let result = document.getElementById('result')
+let FundAmountError = document.getElementById('FundAmountError')
+let InterestError = document.getElementById('InterestError')
+let YearsError = document.getElementById('YearsError')
+let PaymentError = document.getElementById('PaymentError')
 let isok = document.getElementById('isok')
+
+
+let arrayInput = [
+    Fund,
+    Interest,
+    Years,
+    Payment
+]
+let arrayInputerror = [
+    FundAmountError,
+    InterestError,
+    YearsError,
+    PaymentError
+]
+
 
 
 function calc() {
 
     result.value = Number(Fund.value) * (1 + (Number(Interest.value / 100))) / (Number(Years.value) * 12)
-    result.innerHTML = result.value
-    if (Payment.value < result.value) {
 
-        isok.innerHTML = "סכום גבוה, תנסה להקטין את הקרן או תגדיל את התקופה";
+    if (isNaN(Fund) || isNaN(Interest) || isNaN(Payment) || isNaN(Years)) {
+        isok.innerHTML = "";
+
     } else {
-        isok.innerHTML = "הסכום נמוך, הינך יכול לשלם אותו";
+        if (Payment.value < result.value) {
+            result.innerHTML = result.value
+            isok.innerHTML = "סכום גבוה, תנסה להקטין את הקרן או תגדיל את התקופה";
+        } else {
+            result.innerHTML = result.value
+            isok.innerHTML = "הסכום נמוך, הינך יכול לשלם אותו";
+        }
     }
 }
 
-function checkInput(event) {
-    let value = Number(event.target.value)
+for (let i = 0; i < arrayInput.length; i++) {
+
+    arrayInput[i].addEventListener(
+        'keyup',
+        checkInput
+    )
+
+    function checkInput(event) {
+        let value = Number(event.target.value)
+        let error = arrayInputerror[i];
 
 
 
-    if (isNaN(value)) {
-        FundAmount.style.border = '2px solid red'
-        document.getElementById('FundAmountError').innerHTML = 'נא להזין רק מספרים'
-    } else {
-        FundAmount.style.border = ''
-        document.getElementById('FundAmountError').innerHTML = ''
+        if (isNaN(value)) {
+            arrayInput[i].style.border = '2px solid red'
+
+            error.innerHTML = 'נא להזין רק מספרים'
+        } else {
+            arrayInput[i].style.border = ''
+            error.innerHTML = ''
+        }
     }
+
 }
-FundAmount.addEventListener(
-    'change',
-    checkInput
-)
-
-function checkInput2(event) {
-    let value = Number(event.target.value)
-
-
-
-    if (isNaN(value)) {
-        Interest.style.border = '2px solid red'
-        document.getElementById('InterestError').innerHTML = 'נא להזין רק מספרים'
-    } else {
-        Interest.style.border = ''
-        document.getElementById('InterestError').innerHTML = ''
-    }
-}
-Interest.addEventListener(
-    'change',
-    checkInput2
-)
-
-function checkInput3(event) {
-    let value = Number(event.target.value)
-
-
-
-    if (isNaN(value)) {
-        Years.style.border = '2px solid red'
-        document.getElementById('YearsError').innerHTML = 'נא להזין רק מספרים'
-    } else {
-        Years.style.border = ''
-        document.getElementById('YearsError').innerHTML = ''
-    }
-}
-Years.addEventListener(
-    'change',
-    checkInput3
-)
-
-
-function checkInput4(event) {
-    let value = Number(event.target.value)
-
-
-
-    if (isNaN(value)) {
-        Payment.style.border = '2px solid red'
-        document.getElementById('PaymentError').innerHTML = 'נא להזין רק מספרים'
-    } else {
-        Payment.style.border = ''
-        document.getElementById('PaymentError').innerHTML = ''
-    }
-}
-Payment.addEventListener(
-    'change',
-    checkInput4
-)
